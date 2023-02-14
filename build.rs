bc45d4a19b7c443a36b49d7c2b7e822ab3236505
RsSvc <wonderhays@gmail.com>
2023-02-14 16:45:45 +0200

    contracts update

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
 fn main() {
-    // let url = "https://raw.githubusercontent.com/ReachPay/proto-files/main/proto/";
-    // ci_utils::sync_and_build_proto_file(url, "MerchantFlowsGrpc.proto");
-    tonic_build::compile_protos("proto/TradingInfoIntegrationGrpc.proto").unwrap();
+    tonic_build::compile_protos("proto/AccountInfoIntegrationGrpc.proto").unwrap();
 }
